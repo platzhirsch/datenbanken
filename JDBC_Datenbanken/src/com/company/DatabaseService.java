fd8ffc3ea9b5486ea30c27f8cbdbbd3bf5f4a8d5
Felix Flassak <felixflassak@gmail.com>
2020-05-12 20:22:01 +0200

    insertKategorie() added - working

--- JDBC_Datenbanken/src/com/company/DatabaseService.java
+++ JDBC_Datenbanken/src/com/company/DatabaseService.java
@@ -8,16 +8,34 @@ import java.sql.Statement;
 
 public class DatabaseService {
 
+
+
+    public String insertKategorie(String name) throws SQLException, ClassNotFoundException{
+        Connection connection = ConnectionUtils.createNewConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("select * from kat_kategorien where kat_name=\"" + name + "\"");
+
+        if (resultSet.next()) {
+            System.out.println("Kategorie " + resultSet.getString("kat_name")+" existiert bereits");
+        } else {
+            String sql = "INSERT INTO kat_kategorien (kat_name)"
+                    + "VALUES(\"" + name + "\")";
+            statement.executeUpdate(sql);
+            System.out.println("Kategorie " + name + " erstellt");
+        }
+
+        return null;
+    }
+
     public String insertProdukt(String name, Double preis, Integer kat_id) throws SQLException, ClassNotFoundException {
         Connection connection = ConnectionUtils.createNewConnection();
-
         // existiert bereits?
         Statement statement = connection.createStatement();
         ResultSet resultSet = statement.executeQuery("select * from pro_produkte where pro_name=\"" + name + "\" AND pro_preis=\"" + preis + "\" AND pro_kat_id=\"" + kat_id + "\"");
 
         if (resultSet.next()) {
             //existiert
-            System.out.println(resultSet.getString("pro_name") + "existiert bereits");
+            System.out.println("Produkt " + resultSet.getString("pro_name") + " existiert bereits");
         } else {
             //erstellen
             String sql = "Insert INTO pro_produkte (pro_name, pro_preis, pro_kat_id)"
--- JDBC_Datenbanken/src/com/company/Main.java
+++ JDBC_Datenbanken/src/com/company/Main.java
@@ -5,10 +5,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
+    private int anzahlProdukte = 0;
+    private int anzahKunden = 0;
+    private int anzahlGekauft =0;
+    private int anzahlAngesehen =0;
+
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DatabaseService service = new DatabaseService();
-        service.insertProdukt("dildo",12.99,1);
-
+        service.insertProdukt("",10.0,1);
+        service.insertKategorie("");
     }
 }
