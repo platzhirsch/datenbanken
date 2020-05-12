b479021a6e5facc69fa7d18b8e705efbb120802d
Felix Flassak <felixflassak@gmail.com>
2020-05-12 22:02:41 +0200

    TestdatenProdukte erstellt

--- JDBC_Datenbanken/src/com/company/Main.java
+++ JDBC_Datenbanken/src/com/company/Main.java
@@ -5,15 +5,13 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Main {
-    private int anzahlProdukte = 0;
-    private int anzahKunden = 0;
-    private int anzahlGekauft =0;
-    private int anzahlAngesehen =0;
+
 
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         DatabaseService service = new DatabaseService();
-        service.insertProdukt("",10.0,1);
-        service.insertKategorie("");
+        //service.insertProdukt("",10.0,1);
+        //service.insertKategorie("");
+        Testdaten.createProdukte(200);
     }
 }
