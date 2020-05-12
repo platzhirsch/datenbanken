15a42cd7e36ba44f46508c6b9574302606e95547
Felix Flassak <44442249+platzhirsch@users.noreply.github.com>
2020-05-12 19:55:50 +0200

    Update SQLSkript.sql

--- SQLSkript.sql
+++ SQLSkript.sql
@@ -26,7 +26,7 @@ CREATE TABLE pro_produkte (
 pro_name Varchar(50) not null,
 pro_preis decimal(12,2),
 -- technical attributes
-pro_id INTEGER UNIQUE NOT NULL,
+pro_id INTEGER UNIQUE NOT NULL AUTO_INCREMENT,
 pro_kat_id integer UNIQUE NOT NULL,
 -- relations and primary key
 primary key (pro_id),
