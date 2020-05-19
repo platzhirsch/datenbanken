e571b02cb6e034f91cf4bd36b98fdbf691b69b8a
Felix Flassak <44442249+platzhirsch@users.noreply.github.com>
2020-05-19 09:45:05 +0200

    Update SQLStatements.sql

--- SQLStatements.sql
+++ SQLStatements.sql
@@ -1,43 +1,77 @@
+
 -- Wieviele Produkte gibt es in Kategorie X?
-SELECT * FROM pro_produkte WHERE pro_kat_id = ?
+select count(*)
+	from pro_produkte pp
+		join kat_kategorien 
+			on pro_kat_id = kat_id 	
+	where kat_name = "Körperpflege"
+
+
 
 -- Wie oft sieht sich Kunde X ein Produkt an, wie oft kauft er?
-SELECT his_status table his_historie WHERE his_status = ? AND his_kun_id = ? 
+select count(his_status), his_status 
+	from his_historie hh
+		join kun_kundenstamm
+			on hh.his_kun_id = kun_id 
+	where kun_email ="Lavinia.Mcclain@web.de"
+group by his_status 
+		
+
 
 -- Für welche Produkte X interessiert sich Kunde Y?
-SELECT his_pro_id table his_historie WHERE his_kun_id = ? 
+select pro_name, kun_name
+	from his_historie hh 
+		join pro_produkte 
+			on hh.his_pro_id = pro_id 
+		join kun_kundenstamm 
+			on hh.his_kun_id = kun_id
+	where kun_name = "Lavinia Mcclain" and his_status = "Angesehen"
+group by pro_name
+
+
 
 -- Wie groß ist unser Kundenstamm?
-SELECT * FROM kun_kundenstamm
+select count(*)
+	from kun_kundenstamm kk 
 
--- Wieviele Produkte wurden am Tag X gekauft? 
-SELECT * FROM his_historie WHERE his_status = Gekauft
 
 
 -- Welche Produktkategorien hat Kunde mit Email X angesehen?
-select kat_name, pro_name, kun_email
-from his_historie hh 
-inner join pro_produkte 
-	on hh.his_pro_id = pro_id 
-inner join kat_kategorien 
-	on pro_kat_id = kat_id
-inner join kun_kundenstamm 
-	on hh.his_kun_id = kun_id
-where kun_email = "Lavinia.Mcclain@web.de"
+select kat_name, kun_email
+	from his_historie hh 
+		join pro_produkte 
+			on hh.his_pro_id = pro_id 
+		join kat_kategorien 
+			on pro_kat_id = kat_id
+		join kun_kundenstamm 
+			on hh.his_kun_id = kun_id
+	where kun_email = "Lavinia.Mcclain@web.de" and his_status ="Angesehen"
+group by kat_name 
+
+
+
+-- Wieviele Produkte wurden am Tag X gekauft? 
+select count(*)
+	from his_historie hh 
+	where his_status = "Gekauft"
+
 
 
 -- Wie oft wird ein Produkt X angesehen? 
-select count (*)from his_historie hh 
-inner join pro_produkte
-	on hh.his_pro_id = pro_id 
-where his_status = "Angesehen" and pro_name = "Gaming Bildschirm Mars RR296640  Orange"
+select count (*)
+	from his_historie hh 
+		join pro_produkte
+			on hh.his_pro_id = pro_id 
+	where his_status = "Angesehen" 
+	and pro_name = "Gaming Bildschirm Mars RR296640  Orange"
+
 
 
 -- Wie teuer sind Produkte die Kunde kauft/ansieht durchschnittlich?
 select avg(pro_preis), kun_name
-from his_historie hh 
-	inner join pro_produkte 
-		on hh.his_pro_id = pro_id 
-	inner join kun_kundenstamm 
-		on hh.his_kun_id = kun_id 
-where kun_name = "Priscilla Barron"
+	from his_historie hh 
+		join pro_produkte 
+			on hh.his_pro_id = pro_id 
+		join kun_kundenstamm 
+			on hh.his_kun_id = kun_id 
+	where kun_name = "Priscilla Barron"
